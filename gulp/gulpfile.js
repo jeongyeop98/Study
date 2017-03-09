09d4997d4f83336fcbc76ebfb337a0b3dbd0a42a
jeongyeop98 <wjdduqn@gmail.com>
2017-03-09 15:02:20 +0900

    gulp task 분리

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,9 +1,14 @@
 var gulp = require('gulp')
 var uglify = require('gulp-uglify')
 
-gulp.task('default', function() {
-  console.log('default task')
+gulp.task('scripts', function() {
   gulp.src('js/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('build/js'))
 })
+
+gulp.tast('hello', function(){
+  console.log('hello')
+})
+
+gulp.task('default', ['scripts', 'hello'])
