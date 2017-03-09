7918348392e257ed171528bf9e1b8f0e17acbc60
jeongyeop98 <wjdduqn@gmail.com>
2017-03-09 15:07:54 +0900

    gulp Css minify

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp')
 var uglify = require('gulp-uglify')
+var minifyCss = require('gulp-minify-css')
+
+gulp.task('minifyCss', function() {
+  gulp.src('css/*.css')
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist'))
+})
 
 gulp.task('scripts', function() {
   gulp.src('js/*.js')
